85ba8632966c6f2ee08296231cc5141887a15e0f
jonasnordling <jonas.nordling@komut.se>
2016-11-01 16:36:07 +0100

    added button listeners

--- app/src/main/java/se/komut/talasvenska/Player.java
+++ app/src/main/java/se/komut/talasvenska/Player.java
@@ -2,6 +2,9 @@ package se.komut.talasvenska;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.ToggleButton;
 
 public class Player extends AppCompatActivity {
 
@@ -15,11 +18,37 @@ public class Player extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player);
+
+
+
+        final ToggleButton recordButton = (ToggleButton) findViewById(R.id.recordButton);
+        recordButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    startRecording();
+                } else {
+                    stopRecording();
+                }
+            }
+        });
+
     }
 
+    private void startRecording(){
 
+    }
 
+    private void stopRecording(){
 
+    }
+
+    public void listenToSwedish(View v){
+
+    }
+
+    public void listenToYou(View v){
+        
+    }
 
 
 
